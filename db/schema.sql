f8b9b3d014e13396f0b6306783aa409f4e78b475
Mo <mohammadreza@arize.io>
2023-04-21 20:17:38 +0400

    add schema

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  image VARCHAR(255),
+  role VARCHAR(50) DEFAULT 'user'
+);
+
+CREATE TABLE IF NOT EXISTS reservations (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+  start_time TIMESTAMP NOT NULL,
+  end_time TIMESTAMP NOT NULL,
+  UNIQUE (start_time, end_time)
+);
